b8d2b288583cbf75a7827f332a7c3aa8562e0b4f
anyi-sunny <asunnysky65@gmail.com>
2024-06-15 12:47:49 -0500

    Create a basic navigation bar

--- finance-project/src/App.js
+++ finance-project/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <NavBar/>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
