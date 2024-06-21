b2acd9ce5e17012f5468cd11ceabc159a645266e
anyi-sunny <asunnysky65@gmail.com>
2024-06-21 18:45:11 -0500

    Final carousel

--- finance-project/src/Components/Carousel.js
+++ finance-project/src/Components/Carousel.js
@@ -1,34 +1,87 @@
 import React from 'react';
 import '../Style/Carousel.css'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+const colors = ["#59C9A5", "#EF6F6C", "#FFEE93"]
+const delay = 2500;
 
 function Carousel({ images }) {
+    const [index, setIndex] = React.useState(0)
+    const timeoutRef = React.useRef(null);
 
-    const settings = {
-        dots: true, 
-        infinite: true, 
-        autoplay: true,
-        autoplaySpeed: 3500, 
-        pauseOnHover: true, 
-        slidesToShow: 1, 
-        slidesToScroll: 1, 
+    function resetTimeout() {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
     }
 
+    React.useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = 
+        setTimeout(
+            () =>
+                setIndex((prevIndex) =>
+                    prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ), 
+        delay
+        );
+        return () => {
+            resetTimeout();
+        };
+    }, [index]);
+
     return (
-        <div className="shellCarousel">
-            <Slider {...settings} className="slider">
-                {images.map((image, index) => (
-                    <div key={index} className="slideCarousel">
-                        <div className="roundedCarousel">
-                            <img src={image.image} alt="" className="imageCarousel" />
-                        </div>
-                    </div>
+        <div>
+            <div className="slideshow">
+                <div 
+                    className="slideshowSlider"
+                    style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+                >
+                    {colors.map((backgroundColor, index) => (
+                        <div className="slide" key={index} style={{ backgroundColor }}/>
+                    ))}
+                </div>
+            </div>
+            <div className="slideshowDots">
+                {colors.map((_, idx) => (
+                    <div 
+                        key={idx} 
+                        className={`slideshowDot${index === idx ? " active" : ""}`}
+                        onClick={() => {
+                            setIndex(idx);
+                        }}
+                    ></div>
                 ))}
-            </Slider>
+            </div>
         </div>
+        
     )
+
+    // const settings = {
+    //     dots: true, 
+    //     infinite: true, 
+    //     autoplay: true,
+    //     autoplaySpeed: 3500, 
+    //     pauseOnHover: true, 
+    //     slidesToShow: 1, 
+    //     slidesToScroll: 1, 
+    // }
+
+    // return (
+    //     <div className="shellCarousel">
+    //         <Slider {...settings} className="slider">
+    //             {images.map((image, index) => (
+    //                 <div key={index} className="slideCarousel">
+    //                     <div className="roundedCarousel">
+    //                         <img src={image.image} alt="" className="imageCarousel" />
+    //                     </div>
+    //                 </div>
+    //             ))}
+    //         </Slider>
+    //     </div>
+    // )
     // const [imageIndex, setImageIndex] = useState(0)
 
     // function showNextImage() {
