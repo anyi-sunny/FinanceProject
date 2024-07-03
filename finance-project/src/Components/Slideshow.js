cefb2b030d6f6597717226cbde29c10fb5d033b9
kylePellerin <kpellerin@bowdoin.edu>
2024-07-03 13:45:59 -0400

    up

---
+++ finance-project/src/Components/Slideshow.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Slideshow.css'
+
+const colors = ["blue", "red", "red"];
+
+function Slideshow() {
+    return (
+        <div className="slideshow">
+            <div className="slideshowSlider">
+                {colors.map((backgroundColor, index) => (
+                    <div classname="slide" key={index} style={{ backgroundColor }}></div>
+                ))}
+            </div>
+            <div className="slideshowDots">
+                {colors.map((_, idx) => (
+                    <div key={idx} className="slideshowDot"></div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Slideshow;
