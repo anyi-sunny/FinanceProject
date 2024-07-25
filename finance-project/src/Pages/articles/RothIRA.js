bc8f20a5258845171fa30fac1cc52f1306afe12f
anyi-sunny <asunnysky65@gmail.com>
2024-07-24 19:19:50 -0500

    Create RothIRA.js

---
+++ finance-project/src/Pages/articles/RothIRA.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import NavBar from '../../Components/NavBar';
+import Footer from '../../Components/Footer';
+
+
+function CardComp (){
+    return (
+        <div>
+            <NavBar/>
+            <div className='ageShell'>
+                <h1>Roth IRA: the best retirement account you'll ever have</h1>
+                <div className='intro'>
+                    Opening a Roth IRA is the best you can do period. 
+                    This is the best option for a retirement account because you won’t be taxed on your contributions and earnings. 
+                    Every penny you put in and every penny you earn will be all yours when you are ready to withdraw. 
+                    Keep in mind, that you must be at least 59 and a half years old(basically at least 60) to withdraw your earnings without penalty(taxing). 
+                </div>
+                <h2>
+                    How does it work?
+                </h2>
+                <p>
+                    This is how the retirement account works. 
+                    If you know a thing or two about stocks, this is basically another investment account where you can buy and sell stock and all the money that exists in your account will be saved for the sole purpose of when you retire. 
+                    The key thing to keep in mind is when you open and an account and make contributions(put money into the account) your Roth IRA won’t just magically start making money. 
+                    You then have to pick out certain stocks to buy. 
+                </p>
+                <p>
+                    As a younger student, you can afford to buy more aggressive stocks but just make sure you know that the company won’t eventually crash and burn of course. 
+                    Aggressive stocks make more sense when you’re younger is because looking long term, you can afford to weather the fluctuations of these kinds of stocks since you won’t be withdrawing any time soon. 
+                    At the end of the day, you should witness a net gain. 
+                    The reason people don’t buy aggressive stock when they’re older is because the dips and growths of this type of stock is not something that you would want to worry about as you’re about to withdraw your money. 
+                    For example, if I were to invest $500 into an aggressive stock when I’m younger, I may lose $200 or gain $200! 
+                    When I’m older, I do not want to worry about that same risk. 
+                    At that point, you’re better off slowly accumulating earnings off a more popular and expensive stock than risking it all on a wild card. 
+                </p>
+                <h2>
+                    Things to keep in mind
+                </h2>
+                <p>
+                    If that all made sense to you, this is were you think about the type of portfolio you want to have. 
+                    It’s good to have a mix of aggressive and conservative stocks and of course from multiple different companies. 
+                    For retirement accounts especially, you want to think about what’s going to still be here when you retire so invest in stocks that you believe in. 
+                </p>
+                <p>
+                    Now that you know how the Roth IRA works and a little bit about the stock market, here’s everything else you need to know about opening an account and how much you can contribute. 
+                    If you are 18 or older, you can open up a Roth IRA of your own. 
+                    If you are under the age of 18, your parents can open an account for you as early as when you are born. 
+                    Once you are no longer a minor, ownership of the account will be transferred to you. 
+                    In terms of making contributions, you must have an income for that year if you want to contribute. 
+                    Additionally, you will be limited to contributing $7,000 a year while you are under the age of 50(this information was updated 2024). 
+                    If you think about it this way, every year counts. 
+                    That doesn’t mean invest all the money you have right now but it’s good to set aside a reasonable sum that you’d be willing to put away. 
+                </p>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+
+export default CardComp
